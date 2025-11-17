6a5514403332c3f827f955f0f2ef4f349b2e93b2
Andrey Filippov <androsphilippos@gmail.com>
2025-11-17 11:52:28 +0500

    Setting lib mode for vite build

---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { SentenceBuilder } from './exercises/SentenceBuilder';
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,24 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import dts from 'vite-plugin-dts';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    dts({
+      include: ['src'],
+      exclude: ['**/*.stories.tsx', '**/*.test.tsx'],
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      formats: ['es'],
+      fileName: 'index',
+    },
+  },
 });
